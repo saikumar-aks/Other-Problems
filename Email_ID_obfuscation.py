9a0e23da77dbef9f68f106c2fb864574531242ad
Saikumar-ak <81463755+saikumar-aks@users.noreply.github.com>
2021-05-09 12:29:52 +0530

    Update Email_ID_obfuscation.py

--- Email_ID_obfuscation.py
+++ Email_ID_obfuscation.py
@@ -1,18 +1,18 @@
 mail=input()
 li=[]
-li1=[]
 for i in mail:
     if i == '@':
         break
     else:
       li.append(i)
 x=len(li)
-if x <= 5 and len(mail) != x:
+exp =bool(len(mail) != x)
+if x <= 5 and exp:
     for i in li:
         print('*',end="")
     print(mail[x:])
-    
-elif len(li) > 5 and len(mail) != len(li):
+
+elif len(li) > 5 and exp:
     print("***"+mail[3:])
 
 elif len(mail) == len(li):
